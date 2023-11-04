f85acc1ffeedf9ff81b74044550e44627d5d8ac3
Sean.Connor <132519718+scsc22@users.noreply.github.com>
2023-11-04 02:42:55 -0400

    serverJs

---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files
+app.use(express.static('public'));
+
+// Import routes
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+
+// Use routes
+app.use('/', htmlRoutes);
+app.use('/', apiRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
